c9aa225e2f01c55954e56ef371bddb16f3da0937
KJ Jackson <kjjackson619@yahoo.com>
2022-11-22 21:14:57 -0500

    button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
-var config = require('../../public/config');
+//var config = require('../../public/config');
+var searchBtn = document.getElementById('#btn');
+var input = document.getElementById('#input');
 
 var getWeather = function () {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude={part}&appid=" + appId)
+    fetch("https://api.openweathermap.org/data/2.5/onecall?" + "lat=33.44&lon=-94.0" + "4&exclude={part}&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35")
         .then(function (response) {
             response.json().then(function (data) {
                 console.log(data);
@@ -11,5 +13,19 @@ var getWeather = function () {
         });
 
 };
+
+function searchFunction() {
+    if (input == true) {
+        searchBtn.innerHTML = getWeather();
+        return;
+    }
+    else {
+        console.log(error);
+    }
+};
+
+
+
+searchBtn.addEventListener('click', searchFunction);
 
 getWeather();
