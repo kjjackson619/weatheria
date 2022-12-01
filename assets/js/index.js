26a5bda5e3a93f21fde773e5a8c66b748ef7cf19
KJ Jackson <kjjackson619@yahoo.com>
2022-11-30 22:15:41 -0500

    debugging

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,43 +3,45 @@ var searchBtn = document.getElementById('#btn');
 var input = document.getElementById('#input');
 
 // var localStorage = function () {
-//     localStorage.setItem("",)
+
 // };
 
-var getWeatherByCity = function () {
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + "columbus" + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35")
-        .then(function (response) {
-            response.json().then(function (data) {
-                console.log(data);
 
-            });
+// var getWeatherByCity = function () {
 
-        });
+//     let name = input.innerHTML;
 
-};
+//     fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + name + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35")
+//         .then(function (response) {
+//             response.json().then(function (data) {
+//                 console.log(data);
+
+//             });
+
+//         });
+
+// };
 
 var getWeatherByZipCode = function () {
-    fetch("https://api.openweathermap.org/geo/1.0/zip?zip=" + "43230,US" + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35")
+
+    let zip = '43230,us';
+
+    //if (input.innerHTML == zip) {
+
+    fetch("https://api.openweathermap.org/geo/1.0/zip?zip=" + zip + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35")
         .then(function (response) {
             response.json().then(function (data) {
                 console.log(data);
             });
         });
-};
 
-function searchFunction() {
-    if (input == true) {
-        searchBtn.innerHTML = getWeatherByCity();
-        return;
-    }
-    else {
-        console.log(error);
-    }
+    //}
+
 };
 
 
 
-searchBtn.addEventListener('click', searchFunction);
+searchBtn.addEventListener('click', getWeatherByZipCode);
 
-getWeatherByCity();
 getWeatherByZipCode();
+
