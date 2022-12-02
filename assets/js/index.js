0581741507763930a59ffffbb0010a5c3ccf1252
KJ Jackson <kjjackson619@yahoo.com>
2022-12-01 20:51:27 -0500

    fixing errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,25 @@
-//var config = require('../../public/config');
-var searchBtn = document.getElementById('#btn');
-var input = document.getElementById('#input');
+
+var btn = document.getElementById('#btn');
+var inputEl = document.getElementById('#input').val();
 
 // var localStorage = function () {
 
 // };
 
 
-// var getWeatherByCity = function () {
+const formHandler = function () {
 
-//     let name = input.innerHTML;
+    var getWeatherByCity = "https://api.openweathermap.org/geo/1.0/direct?q=" + inputEl + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35"
 
-//     fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + name + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35")
-//         .then(function (response) {
-//             response.json().then(function (data) {
-//                 console.log(data);
-
-//             });
-
-//         });
-
-// };
-
-var getWeatherByZipCode = function () {
-
-    let zip = '43230,us';
-
-    //if (input.innerHTML == zip) {
-
-    fetch("https://api.openweathermap.org/geo/1.0/zip?zip=" + zip + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35")
-        .then(function (response) {
-            response.json().then(function (data) {
-                console.log(data);
-            });
-        });
-
-    //}
-
-};
+    var getWeatherByZipCode = "https://api.openweathermap.org/geo/1.0/zip?zip=" + inputEl + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35"
 
 
 
-searchBtn.addEventListener('click', getWeatherByZipCode);
+}
 
-getWeatherByZipCode();
+
+
+
+btn.addEventListener('click', formHandler);
+
 
--- public/config.js
+++
@@ -1,3 +0,0 @@
-var appId = '2cf57f3bc628ca68fe4f4198c9697a35'
-
-module.exports = appId;
