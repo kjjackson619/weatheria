420679323a935a4a56ee26c01a8b4d999562b89f
KJ Jackson <kjjackson619@yahoo.com>
2022-12-01 21:16:29 -0500

    condense code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 
-var btn = document.getElementById('#btn');
-var inputEl = document.getElementById('#input').val();
+var buttonEl = document.getElementById('#btn');
+var inputEl = document.getElementById('#input');
 
 // var localStorage = function () {
 
@@ -11,15 +11,23 @@ const formHandler = function () {
 
     var getWeatherByCity = "https://api.openweathermap.org/geo/1.0/direct?q=" + inputEl + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35"
 
-    var getWeatherByZipCode = "https://api.openweathermap.org/geo/1.0/zip?zip=" + inputEl + "&appid=" + "2cf57f3bc628ca68fe4f4198c9697a35"
-
+    if (inputEl == "") {
+        console.log(inputEl)
+    } else {
+        async () => ({
+            url: getWeatherByCity,
+            method: 'GET'
+        }).then(function (response) {
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        });
+    };
 
 
 }
 
 
 
-
-btn.addEventListener('click', formHandler);
-
+buttonEl.addEventListener("click", formHandler);
 
